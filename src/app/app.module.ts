b4918e994e0be57b62536d3ed93189f199fc0ad9
Artem Abramov <Artem_Abramov1@epam.com>
2022-10-30 20:09:48 +0300

    added first components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchbarComponent } from './components/searchbar/searchbar.component';
+import { CourseCardComponent } from './components/course-card/course-card.component';
+import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    SearchbarComponent,
+    CourseCardComponent,
+    BreadcrumbsComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
