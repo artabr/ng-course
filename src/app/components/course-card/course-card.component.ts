9e4bdbf759bb3b56ebfef9a89c1e48fb4891371e
Artem Abramov <Artem_Abramov1@epam.com>
2022-12-07 22:03:28 +0300

    new page

--- src/app/components/course-card/course-card.component.ts
+++ src/app/components/course-card/course-card.component.ts
@@ -1,10 +1,11 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { Course } from '../../types/types';
 
 @Component({
   selector: 'app-course-card',
   templateUrl: './course-card.component.html',
   styleUrls: ['./course-card.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CourseCardComponent {
   @Input() course?: Course;
